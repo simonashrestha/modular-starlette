b9a9078351ba99cc6d0b4afe5170c14cad7a45b4
simonashrestha <simonashrestha1@gmail.com>
2024-08-21 12:40:09 +0545

    recent

--- Blogs/Like/likequeries.py
+++ Blogs/Like/likequeries.py
@@ -57,65 +57,6 @@ async def update_reactions(user_id: int, blog_id: int, new_reaction: str):
     return f"Blog {new_reaction}d successfully"
 
 
-# async def increment_likes(user_id: int, blog_id: int, reaction: str):
-#     # Fetch the current reactions for the blog
-#     query = select(blog.c.reactions).where(blog.c.blog_id == blog_id)
-#     current_reactions = await database.fetch_one(query)
-    
-#     if not current_reactions:
-#         return "Blog not found"
-    
-#     reactions = current_reactions["reactions"]
-#     if reactions is None:
-#         reactions = {}
-
-#     # Check if the user has already liked the blog
-#     if reactions.get(user_id) == reaction:
-#         return "Already liked"
-
-#     # Update the reactions
-#     reactions[user_id] = reaction
-
-#     update_query = (
-#         update(blog)
-#         .where(blog.c.blog_id == blog_id)
-#         .values(
-#             likes=func.coalesce(blog.c.likes, 0) + 1,
-#             reactions=reactions,
-#         )
-#     )
-#     await database.execute(update_query)
-
-#     return "Liked"
-
-# async def increment_dislikes(user_id: int, blog_id: int, reaction: str):
-#     query= select(blog.c.reactions).where(blog.c.blog_id==blog_id)
-#     current_reactions= await database.fetch_one(query)
-
-#     if not current_reactions:
-#         return "Blog not found"
-    
-#     reactions= current_reactions["reactions"]
-#     if reactions is None:
-#         reactions= {}
-
-#     if reactions.get(user_id)==reaction:
-#         return "Already disliked"
-    
-#     reactions[user_id]= reaction
-
-#     update_query= (
-#         update(blog)
-#         .where(blog.c.blog_id== blog_id)
-#         .values(
-#             dislikes=  func.coalesce(blog.c.dislikes, 0)+1,
-#             reactions= reactions,
-#         )
-#     )
-    
-#     await database.execute(update_query)
-
-#     return "Disliked"
 
 
 
--- Email_Verification/email_worker.py
+++ Email_Verification/email_worker.py
@@ -29,29 +29,6 @@ class WorkerSettings:
     on_shutdown = shutdown
 
 
-# from arq import Worker, create_pool
-# from arq.connections import RedisSettings
-# from Email_Verification.email_tasks import send_verification_email
-
-# REDIS_SETTINGS = RedisSettings(
-#     host='localhost',
-#     port=6379,
-#     password=None 
-# )
-
-# async def startup(ctx):
-#     print("Worker starting up..")
-#     ctx['redis']= await create_pool(RedisSettings())
-#     print("Connected to Redis.")
-
-# async def shutdown(ctx):
-#     print("Worker shutting down..")
-
-# class WorkerSettings:
-#     redis_settings= RedisSettings()
-#     functions= [send_verification_email]
-#     on_startup= startup
-#     on_shutdown= shutdown
 
     
 
